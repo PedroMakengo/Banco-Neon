66759858bce438a364d7dc8ae3ade9345c199986
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-04 11:21:03 -0300

    Trabalhando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
-import { Container, ContentHeader, Content } from "./style";
+import { Container, ContentHeader, Content, Banner } from "./style";
 
 import logo from "../../assets/images/logo-neon-color.svg";
+import group141 from "../../assets/images/Group141.svg";
 
 export function Header() {
   return (
@@ -17,9 +18,32 @@ export function Header() {
           </nav>
         </Content>
       </ContentHeader>
-      <div>
-        <span>Abra sua conta, é só baixar o app!</span>
-      </div>
+      <Banner>
+        <div className="account">
+          <span>Abra sua conta, é só baixar o app!</span>
+          <h1>A conta digital certa pra cuidar bem do seu dinheiro</h1>
+          <button>Abra sua contan digital</button>
+
+          <div>
+            <div>
+              <img src="" alt="" />
+              <span>Cartão sem anuidade</span>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <span>Conta digital 100% grátis</span>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <span>Seu dinheiro rendendo mais</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="digital">
+          <h1>Banco 100% digital</h1>
+        </div>
+      </Banner>
     </Container>
   );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   width: 100%;
-  height: 80vh;
+  height: 90vh;
   background: url("../../assets/images/bg-hero.svg");
 
   background-size: 100%;
@@ -44,6 +44,62 @@ export const Content = styled.div`
         border-radius: 50px;
         padding: 0.6rem 1.5rem;
       }
+    }
+  }
+`;
+
+export const Banner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  height: 80%;
+
+  display: flex;
+  align-items: center;
+
+  color: #fff;
+
+  .account {
+    width: 50%;
+
+    > span {
+      display: block;
+      margin-bottom: 1.5rem;
+
+      font-size: 1.1rem;
+
+      color: #b2e7f8;
+    }
+
+    h1 {
+      font-size: 3rem;
+      line-height: 3.5rem;
+
+      margin-bottom: 1.5rem;
+    }
+
+    button {
+      background: var(--yellow);
+      padding: 0.8rem 1rem;
+
+      border-radius: 50px;
+      border: 0;
+
+      color: #855b35;
+
+      margin-bottom: 1.5rem;
+    }
+  }
+
+  .digital {
+    background-image: url("../../assets/images/Group141.svg");
+    background-position: center;
+    h1 {
+      font-size: 3rem;
+      color: #b2e7f8;
+    }
+
+    img {
+      width: 40%;
     }
   }
 `;
