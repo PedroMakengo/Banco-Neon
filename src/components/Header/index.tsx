a0aed7ac2b8369a1db49ce3cd8fd2295715761c0
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-04 10:40:10 -0300

    Trabalhando no projeto neon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
-import { Container } from "./style";
+import { Container, Content } from "./style";
+
+import logo from "../../assets/images/logo-neon-color.svg";
 
 export function Header() {
   return (
     <Container>
-      <div>
-        <h1>Olá Mundo</h1>
-      </div>
+      <Content>
+        <img src={logo} alt="Logo" />
+        <nav>
+          <a href="#produto">Produtos</a>
+          <a href="#conta">Conta Digital PJ</a>
+          <a href="#quemsomos">Quem Somos</a>
+          <a href="#ajuda">Ajuda</a>
+          <a href="#abra">Abra a sua conta digital</a>
+        </nav>
+      </Content>
     </Container>
   );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,8 +2,38 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   width: 100%;
-  height: 60vh;
+  height: 70vh;
   background: url("../../assets/images/bg-hero.svg");
 
   background-size: 100%;
+  background-color: var(--blue);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  height: 7rem;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    width: 120px;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    a {
+      text-decoration: none;
+      color: #fff;
+      font-weight: 100;
+
+      & + a {
+        margin-left: 2rem;
+      }
+    }
+  }
 `;
