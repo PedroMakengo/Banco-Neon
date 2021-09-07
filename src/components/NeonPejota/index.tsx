0273ce95f8aeba58f980b6613858a3e59a8d9618
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-07 14:29:32 -0300

    Terminando

--- src/components/NeonPejota/index.tsx
+++ src/components/NeonPejota/index.tsx
@@ -1,10 +1,41 @@
 import { Container, Content } from "./style";
 
+import { FaApple } from "react-icons/fa";
+
+import BaixarImg from "../../assets/images/Group 9.svg";
+
 export function NeonPejota() {
   return (
     <Container>
       <Content>
-        <h1>Olá Mundo</h1>
+        <div className="BaixaApp">
+          <img src={BaixarImg} alt="" />
+
+          <div className="conteudoApp">
+            <h2>Baixe nosso app</h2>
+            <p>
+              Que tal abrir uma conta digital para ver como a gente faz a sua
+              vida muito mais simples
+            </p>
+            <div className="buttons">
+              <button>
+                <FaApple />
+                <div>
+                  <small>Download via</small>
+                  <span>Apple Store</span>
+                </div>
+              </button>
+              <button>
+                <FaApple />
+                <div>
+                  <small>Download via</small>
+                  <span>Google Play</span>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="NeonBanner"></div>
       </Content>
     </Container>
   );
--- src/components/NeonPejota/style.ts
+++ src/components/NeonPejota/style.ts
@@ -1,14 +1,87 @@
 import styled from "styled-components";
 
+import Banner from "../../assets/images/banner-pj.png";
+import Union from "../../assets/images/Union-7.svg";
+
 export const Container = styled.section`
-  height: 80vh;
+  height: 70vh;
 `;
 
 export const Content = styled.div`
   max-width: 1100px;
+  height: 100%;
   margin: 0 auto;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  background-image: url(${Union});
+  background-repeat: no-repeat;
+  background-position-x: 10rem;
+  background-position-y: 1rem;
+
+  padding-top: 8rem;
+
+  .BaixaApp {
+    width: 30%;
+    text-align: center;
+    margin-top: -1.5rem;
+
+    .conteudoApp {
+      margin-top: -2rem;
+    }
+
+    p {
+      font-weight: 300;
+      color: #ccc;
+    }
+
+    .buttons {
+      display: flex;
+      flex-direction: column;
+      width: 60%;
+
+      margin: 0 auto;
+      margin-top: 2rem;
+
+      button {
+        padding: 0.6rem 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 15px;
+        border: 0;
+
+        background-color: #33303e;
+        color: #fff;
+
+        svg {
+          font-size: 2rem;
+          margin-right: 1rem;
+        }
+        & + button {
+          margin-top: 1rem;
+        }
+
+        div {
+          display: flex;
+          flex-direction: column;
+
+          span {
+            font-weight: 400;
+          }
+        }
+      }
+    }
+  }
+
+  .NeonBanner {
+    width: 64%;
+    height: 100%;
+    background-image: url(${Banner});
+    background-repeat: no-repeat;
+    background-size: 100%;
+  }
 `;
