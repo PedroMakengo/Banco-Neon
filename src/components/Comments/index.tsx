9e70570821bc1e9c55e19bedb679c2bde08a5b88
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-07 15:44:52 -0300

    Terminando a sessão

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,9 +1,50 @@
-import { Comment } from "./style";
+import { Comment, Content } from "./style";
 
 export function Comments() {
   return (
     <Comment>
-      <h1>Olá Mundo</h1>
+      <Content>
+        <div className="content-top">
+          <div className="titles">
+            <h1>Prático, fácil, moderno</h1>
+            <h2>Você resolve tudo sem estresse</h2>
+          </div>
+          <div className="vetor"></div>
+        </div>
+
+        <div className="comments">
+          <div className="comment">
+            <div className="comment-header">
+              <span>@marciogurka</span>
+              <img src="" alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              tempore repellat consectetur
+            </p>
+          </div>
+          <div className="comment">
+            <div className="comment-header">
+              <span>@marciogurka</span>
+              <img src="" alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              tempore repellat consectetur
+            </p>
+          </div>
+          <div className="comment">
+            <div className="comment-header">
+              <span>@marciogurka</span>
+              <img src="" alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              tempore repellat consectetur
+            </p>
+          </div>
+        </div>
+      </Content>
     </Comment>
   );
 }
--- src/components/Comments/style.ts
+++ src/components/Comments/style.ts
@@ -1,3 +1,62 @@
 import styled from "styled-components";
 
-export const Comment = styled.section``;
+export const Comment = styled.section`
+  height: 50vh;
+`;
+
+export const Content = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+
+  flex-direction: column;
+
+  .content-top {
+    display: flex;
+    justify-content: space-between;
+
+    h1 {
+      font-size: 2rem;
+      font-weight: bold;
+      color: #ccc;
+    }
+    h2 {
+      font-weight: bold;
+      font-size: 2rem;
+    }
+  }
+
+  .comments {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 2rem;
+
+    .comment {
+      width: 50%;
+      background: #f7f7fa;
+
+      padding: 1.5rem;
+      border-radius: 0.5rem;
+
+      & + .comment {
+        margin-left: 1.5rem;
+      }
+
+      span {
+        font-weight: 500;
+      }
+
+      p {
+        width: 280px;
+        font-size: 0.9rem;
+        font-weight: 300;
+        margin-top: 1rem;
+      }
+    }
+  }
+`;
