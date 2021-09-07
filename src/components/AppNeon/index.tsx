398e651236f0b48d15bbcba4a53f0d264527c3a7
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-07 10:40:26 -0300

    Adicionando o react-icons

--- src/components/AppNeon/index.tsx
+++ src/components/AppNeon/index.tsx
@@ -7,6 +7,18 @@ export function AppNeon() {
         <div></div>
         <div>
           <h1>Resolva sua vida direto pelo app Neon!</h1>
+          <div className="itens">
+            <div>
+              <img src="" alt="" />
+              <div>
+                <h2>Cartão de crédito sem anuidade</h2>
+                <span>
+                  Conta digital com cartão de crédito sem anuidade e sem
+                  complicações
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </ContentApp>
     </SectionApp>
