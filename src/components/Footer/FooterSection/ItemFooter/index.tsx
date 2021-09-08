2dab724a24736121c7de363887dda1b1db9b7441
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-08 06:55:19 -0300

    Melhorando os itens do footer

--- src/components/Footer/FooterSection/ItemFooter/index.tsx
+++ src/components/Footer/FooterSection/ItemFooter/index.tsx
@@ -1,13 +1,16 @@
-export function ItemFooter() {
+interface ItemFooterProps {
+  title?: string;
+  data?: string[];
+}
+export function ItemFooter({ data, title }: ItemFooterProps) {
+  console.log(data);
   return (
     <div className="footer-item">
-      <h2>Produtos Neon</h2>
+      <h2>{title}</h2>
       <nav>
-        <a href="#fasf">Cartão de Crédito</a>
-        <a href="#fasf">Cartão pré-pago</a>
-        <a href="#fasf">Neon mais</a>
-        <a href="#fasf">Investimentos</a>
-        <a href="#fasf">Empréstimo</a>
+        {data?.map((e) => (
+          <a href="data">{e}</a>
+        ))}
       </nav>
     </div>
   );
--- src/components/Footer/FooterSection/index.tsx
+++ src/components/Footer/FooterSection/index.tsx
@@ -3,7 +3,17 @@ import { ItemFooter } from "./ItemFooter";
 export function FooterSection() {
   return (
     <div className="footer-section">
-      <ItemFooter />
+      <ItemFooter
+        title="Produtos Neon"
+        data={[
+          "Cartão de crédito",
+          "Cartão pré-pago",
+          "Neon Mais",
+          "Investimentos",
+          "Empréstimo",
+        ]}
+      />
+      <ItemFooter title="Cartão digital PJ" data={["Sou MEI", "Sou MEI"]} />
     </div>
   );
 }
